145ba49f66d02de73fa967cab80c51b25f9769d5
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-12-11 01:31:49 -0600

    Runge Kutta so easy.

--- RK4/RK4.fs
+++ RK4/RK4.fs
@@ -2,4 +2,10 @@
 
 module ODE =
 
-    let rk4 f x = (fun t -> 3.0)
+    let rk4 x' (t0,x0) t =
+        let h = t-t0
+        let k1 = h * (x' (t0,x0))
+        let k2 = h * (x' (t0 + h/2.0, x0 + k1/2.0))
+        let k3 = h * (x' (t0 + h/2.0, x0 + k2/2.0))
+        let k4 = h * (x' (t0 + h, x0 + k3))
+        x0 + (k1 + 2.0 * (k2 + k3) + k4)/6.0
--- RK4/Tests.fsx
+++ RK4/Tests.fsx
@@ -1,8 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "#RK4.fs"
-open RK4
+#load "RK4.fs"
+open NumericalAnalysis.ODE
+open System
 
 // Define your library scripting code here
 
+//let f = rk4 (fun (t,x) -> x + Math.Pow(Math.E, t)) (0.0, 2.0)
+//
+//f 0.5
+
+let f' (t,_) = 
+    let sin = Math.Sin(t)
+    Math.Sqrt(1.0 - sin * sin / 4.0)
+
+let f1 = rk4 f' (30.0*Math.PI/180.0, 0.51788193)
+let f2 = rk4 f' (90.0*Math.PI/180.0, 1.46746221)
+
+for i = 0 to 90 do 
+    let rads = float(0) * Math.PI / 180.0
+    printfn "%.8f\t%.8f\t%.8f" rads (f1 rads) (f2 rads)
